57932b35c16486da93c962bad38d4e836d909f3e
Simon Let <simon.a.let@gmail.com>
2017-12-02 13:30:04 +0100

    Day 01 captcha - part2

--- src/day-01-captcha.py
+++ src/day-01-captcha.py
@@ -5,14 +5,12 @@ from sys import argv
 with open(argv[1]) as f:
     captcha = f.readline().strip()
     print('CAPTCHA input: {0}'.format(captcha))
-    captcha += captcha[0]
 
     result = 0
-    prev_char = None
-    for char in captcha:
-        if prev_char == char:
+    for i, char in enumerate(captcha):
+        x = int(i +  len(captcha) / 2) % len(captcha)
+        if captcha[i] == captcha[x]:
             result += int(char)
-        prev_char = char
 
     print('CAPTCHA result: {0}'.format(result))
 
