5478f00ab0865ebf816d169bcb4a9dabfce4bd19
Simon Let <simon.a.let@gmail.com>
2017-12-02 13:47:00 +0100

    Day 02 checksum - part2

--- src/day-02-checksum.py
+++ src/day-02-checksum.py
@@ -1,6 +1,18 @@
 
 from sys import argv
 
+def find_one_divides_the_other(cols):
+    cols = sorted(cols)
+    for x, value_x in enumerate(cols):
+        for y, value_y in enumerate(cols):
+            if x == y:
+                continue
+            if value_x % value_y == 0:
+                return value_x, value_y
+            if value_y % value_x == 0:
+                return value_y, value_x
+
+    assert False 
 
 with open(argv[1]) as f:
     checksum = 0
@@ -11,14 +23,12 @@ with open(argv[1]) as f:
         cols = list(map(lambda x : int(x), cols))
         print('values: {0}'.format(cols))
 
-        min_val = min(cols)  
-        max_val = max(cols) 
-        print('min: {0}'.format(min_val))
-        print('max: {0}'.format(max_val))
+        val_x, val_y = find_one_divides_the_other(cols)
 
-        row_checksum = max_val - min_val
+        row_checksum = val_x / val_y
         checksum += row_checksum 
         print('row CHECKSUM: {0}'.format(row_checksum))
 
+    checksum = int(checksum)
     print('\nspreadsheet CHECKSUM: {0}'.format(checksum))
 
